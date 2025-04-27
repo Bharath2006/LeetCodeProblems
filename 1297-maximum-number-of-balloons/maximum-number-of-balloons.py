49564b5a26ce9d68aefae112525732be86065a5b
Bharath <86903043+Bharath2006@users.noreply.github.com>
2025-04-27 20:54:19 +0530

    Time: 3 ms (60.74%) | Memory: 17.8 MB (75.97%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        text = text.lower()
+        mp: dict = {}
+        mp["b"] = 0
+        mp["a"] = 0
+        mp["l"] = 0
+        mp["o"] = 0
+        mp["n"] = 0
+        for txt in text:
+            if txt in mp:
+                mp[txt] += 1
+        return min(mp["b"], mp["a"], mp["l"]//2, mp["o"]//2, mp["n"])
